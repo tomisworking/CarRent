fe04736351a16d980ecb29bb45138a348cf5ccdf
Tom <123108269+tomisworking@users.noreply.github.com>
2025-08-30 17:58:08 +0200

    Update settings.py

--- exotic_rental/settings.py
+++ exotic_rental/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w6d^wf)1o^5(^o#*6yqr5o7lt)2qnq%h0_94ue(zd^2np4)p+('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
